71d51ebb561982647c39d9aa1eead8824cb3db4f
Kris Salmi <kristiina.k.salmi@gmail.com>
2021-09-16 11:41:51 +0300

    new dynamic version with print colors and rounding analysis

--- weshare_dynamic.py
+++ weshare_dynamic.py
@@ -3,6 +3,7 @@
 # for each participant.
 
 import sys
+from decimal import *
 
 option_p = False
 
@@ -19,17 +20,17 @@ class bcolors:
     UNDERLINE = '\033[4m'
 
 def display_usage():
-  print("Usage: weshare_dynamic.py person_1 [contribution in euros] person_2 ... \
-    -p x% person_who_pays_less [contribution in euros]")
-  print("Write name of person followed by the amount that person contributed to the \
-    overall cost of the shared event. If a person made multiple payments, those can be \
-    added as separate arguments. People who did not contribute to the costs must \
-    have the number 0 as their payment.")
-  print("Option '-p' precedes the info of the person who is seen to have to \
-    contribute less than other people. '-p' option is followed by the percentage \
-    of what that person has to pay compared to other people. This is followed by \
-    the name of the person and the amount they contributed.")
-  print("Example usage: \nAdam 350 Bertha 10 30 47 Cecilia 0 -p 50% David 0")
+  print("\nUsage: weshare_dynamic.py person_1 [contribution in euros] person_2 ...\
+  -p x% person_who_pays_less [contribution in euros]\n")
+  print("Write name of person followed by the amount that person contributed to the\
+  overall cost of the shared event. If a person made multiple payments, those can be\
+  added as separate arguments. People who did not contribute to the costs must\
+  have the number 0 as their payment.")
+  print("\nOption '-p' precedes the info of the person who is seen to have to\
+  contribute less than other people. '-p' option is followed by the percentage\
+  of what that person has to pay compared to other people. This is followed by\
+  the name of the person and the amount they contributed.")
+  print("\nExample usage: \npythn weshare_dynamic.py Adam 350 Bertha 10 30 47 Cecilia 0 -p 50% David 0")
   exit()
 
 def check_if_number(n):
@@ -38,7 +39,7 @@ def check_if_number(n):
     except ValueError:
         return False
     else:
-        return float(n)
+        return Decimal(n)
 
 def save_person_who_pays_less(args, paying_less):
   for index, arg in enumerate(args):
@@ -47,7 +48,7 @@ def save_person_who_pays_less(args, paying_less):
         display_usage()
       paying_less.append(args[index + 2])
       # convert percentage into a float number
-      percentage = float(args[index + 1][:-1]) / 100
+      percentage = Decimal(args[index + 1][:-1]) / 100
       paying_less.append(percentage)
       args.pop(index + 1)
       args.pop(index)
@@ -70,7 +71,7 @@ def handle_command_line_args(args):
       name = args[i]
       while (j <= len_arg):
         num = check_if_number(args[j])
-        if not num:
+        if num is False:
           if (j == i + 1):
             print("Error, all names must be followed by a number")
             exit()
@@ -90,26 +91,41 @@ def handle_command_line_args(args):
 
 
 def calc_remaining_costs_per_person(payments, paying_less):
-  all_costs = sum(payments.values())
+  all_costs = (sum(payments.values())).quantize(Decimal("1.00"))
+
   print("\nSum of all payments is " + bcolors.OKBLUE + str(all_costs) + bcolors.ENDC + " euros.")
   num_of_people = len(payments) - 1 + paying_less[1] if option_p == True else len(payments)
   share_per_person = all_costs / num_of_people
-  print("Share per person is " + bcolors.OKBLUE + str(round(share_per_person, 2)) + bcolors.ENDC + " euros.")
+  share_per_person = share_per_person.quantize(Decimal("1.00"))
+  print("Share per person is " + bcolors.OKBLUE + str(share_per_person) + bcolors.ENDC + " euros.")
 
   if option_p:
-    share_of_person_paying_less = share_per_person * paying_less[1]
+    share_of_person_paying_less = (share_per_person * Decimal(paying_less[1])).quantize(Decimal("1.00"))
     print("The share of " + bcolors.OKCYAN + paying_less[0] + bcolors.ENDC + " is " + \
-      bcolors.OKBLUE + str(round(share_of_person_paying_less, 2)) + bcolors.ENDC + " euros.")
+      bcolors.OKBLUE + str(share_of_person_paying_less) + bcolors.ENDC + " euros.")
 
-  for person, share in payments.items():
+  for person in payments:
     if option_p and person == paying_less[0]:
       payments[person] -= share_of_person_paying_less
     else:
       payments[person] -= share_per_person
 
-def print_final_payments(trio):
-  print(bcolors.OKCYAN + trio[1] + bcolors.ENDC + " will pay " + bcolors.OKCYAN + trio[0] + \
-    bcolors.ENDC + ' ' + bcolors.OKGREEN +  str(round(trio[2], 2)) + bcolors.ENDC + " euros")
+def print_final_payments(completed):
+
+  print("\nPayments to be made:")
+  for trio in completed:
+    print(bcolors.OKCYAN + trio[1] + bcolors.ENDC + " will pay " + bcolors.OKCYAN + trio[0] + \
+    bcolors.ENDC + ' ' + bcolors.OKGREEN +  str(trio[2].quantize(Decimal("1.00"))) + bcolors.ENDC + " euros")
+
+def print_remaining(payments):
+  print("\nBecause of rounding, costs can not be shared 100% equally:")
+  for person, costs in payments.items():
+    print(bcolors.OKCYAN + person + bcolors.ENDC + " will end up paying ", end = " ")
+    if costs < 0:
+      print(bcolors.WARNING + str(costs * -1) + bcolors.ENDC + " euros too little.")
+    else:
+      print(bcolors.WARNING + str(costs) + bcolors.ENDC + " euros too much.")
+  print("")
 
 def main():
   paying_less = []
@@ -136,35 +152,32 @@ def main():
 
     i = 0
     while i < len(payments):
-      if (sorted_min[i][1] < 0):
-        if sorted_max[i][0] == sorted_min[i][0]:
-          break
+      if (sorted_min[i][1] < 0 and sorted_max[i][1] > 0):
         if sorted_max[i][1] + sorted_min[i][1] >= 0.0:
           del payments[sorted_min[i][0]]
-          payments[sorted_max[i][0]] = sorted_max[i][1] + sorted_min[i][1]
+          if sorted_max[i][1] + sorted_min[i][1] == 0.0:
+            del payments[sorted_max[i][0]]
+          else:
+            payments[sorted_max[i][0]] = sorted_max[i][1] + sorted_min[i][1]
           temp = [sorted_max[i][0], sorted_min[i][0], (sorted_min[i][1] * -1)]
           completed.append(temp)
         else:
-          payments[sorted_min[i][0]] = round(sorted_min[i][1] + sorted_max[i][1], 2)
+          payments[sorted_min[i][0]] = sorted_min[i][1] + sorted_max[i][1]
           del payments[sorted_max[i][0]]
           temp = [sorted_max[i][0], sorted_min[i][0], sorted_max[i][1]]
           completed.append(temp)
       i += 1
 
-  # This extra check is to deal with floating point "inconsistency", because
-  #  even though the sum of 2 floating points should be exactly 0.0 (and thus
-  # should have fallen into the first if clause), it doesn't always register
-  if payments:
-    if payments.values()[0] == 0.0:
-      payments.clear()
-  print("\nPayments to be made:")
-  for trio in completed:
-    print_final_payments(trio)
-  print("")
+  print_final_payments(completed)
 
   if (payments):
-    print(payments)
-    print("Because of rounding, " + str(round(payments.values()[0], 2)) + " euros will be lost.\n")
+    print_remaining(payments)
+
+
+    # print(payments)
+    # payment_values = payments.values()
+    # lost_money = list(payment_values)[0]
+    # print("Because of rounding, " + str(lost_money.quantize(Decimal('1.00'))) + " euros will be lost.\n")
 
 if __name__ == "__main__":
   main()
